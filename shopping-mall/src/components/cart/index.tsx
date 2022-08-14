4e7e4e67e5f5ee388b7d131a2260d0a6368696cc
yjh-1008 <exesea12@gmail.com>
2022-08-14 21:39:51 +0900

    3-2

--- shopping-mall/src/components/cart/index.tsx
+++ shopping-mall/src/components/cart/index.tsx
@@ -1,16 +1,42 @@
+import { createRef, forwardRef, SyntheticEvent, useRef } from "react";
 import {CartType} from "../../graphql/cart"
 import CartItem from "./item"
 
 const CartList = ({items}:{items : CartType[]})=>{
+  const formRef = useRef<HTMLFormElement>(null);
+  const checkboxRefs = items.map(()=> createRef<HTMLInputElement>());
+
+  const handelCheckboxChanged = (e:SyntheticEvent) => {
+    if (!formRef.current) return;
+    // const checkboxes = formRef.current.querySelectorAll<HTMLInputElement>(".cart-item__checkbox");
+    const targetInput = e.target as HTMLInputElement;
+    const data = new FormData(formRef.current);
+    const selectedCount = data.getAll("select-item").length;
+
+    if (targetInput.classList.contains('select-all')) {//전체선택 선택지
+      const allChecked = targetInput.checked;
+      checkboxRefs.forEach((inputElem) => {
+        inputElem.current!.checked = allChecked
+      });
+    }else{
+      //개별 선택지
+      const allChecked = selectedCount === items.length;
+      formRef.current.querySelector<HTMLInputElement>(".select-all")!.checked =
+        allChecked;
+    }
+  }
+
     return (
-      <>
-      <label><input type="checkbox" />전체 선택</label>
-        <ul className="cart">
-          {items.map((item) => (
-            <CartItem {...item} key={item.id} />
-          ))}
-        </ul>
-      </>
+      <form ref={formRef} onChange={handelCheckboxChanged}>
+          <label>
+            <input className="select-all" type="checkbox" />전체 선택
+          </label>
+          <ul className="cart">
+            {items.map((item,i) => (
+              <CartItem {...item} key={item.id} ref={checkboxRefs[i]} />
+            ))}
+          </ul>
+      </form>
     );
 }
 
